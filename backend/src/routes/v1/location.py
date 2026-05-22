b99cc98db0e69268d370fab66435e97e4aecee09
Sangale Piyush Rajendra <sangalepiyush.rajen@hcltech.com>
2026-05-22 11:08:31 +0530

    combined getalllocation and getlocation api

--- backend/src/routes/v1/location.py
+++ backend/src/routes/v1/location.py
@@ -16,18 +16,22 @@ from src.utils.auth.auth import require_permission
 router = APIRouter()
 
 
-@router.get("", response_model=LocationsResponse, dependencies=[Depends(require_permission("location:read"))])
+@router.get(
+    "", 
+    response_model=LocationsResponse, 
+    dependencies=[Depends(require_permission("location:read"))]
+)
+@router.get(
+    "/{location_id}", 
+    response_model=LocationResponse, 
+    dependencies=[Depends(require_permission("location:read"))]
+)
 def get_locations(
     request: Request,
     db: Session = Depends(get_db),
-) -> LocationsResponse:
-    return LocationsResponse(locations=get_all_location_details(db, request.state.lang))
-
-
-@router.get("/{location_id}", response_model=LocationResponse, dependencies=[Depends(require_permission("location:read"))])
-def get_location(
-    location_id: int,
-    request: Request,
-    db: Session = Depends(get_db),
-) -> LocationResponse:
-    return get_location_details(db, location_id, request.state.lang)
+    location_id: int | None = None,
+):
+    if location_id is not None:
+        return get_location_details(db, location_id, request.state.lang)
+    else:
+        return LocationsResponse(locations=get_all_location_details(db, request.state.lang))
