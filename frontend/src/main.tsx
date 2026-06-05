4a5ce4f731e3a8df10b450a90f6bad5fd173c168
Harsh Singh <kritihcl>
2026-06-05 15:25:40 +0530

    Update Lohin Design

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,9 +7,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from '../src/components/ui/ToastProvider';
 
 // Styles
-import '../src/assets/Style/index.css';
-import '../src/assets/Style/auth.css';
-import '../src/assets/Style/layout.css';
+import '@fontsource/material-icons/index.css';
+import './index.css';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
